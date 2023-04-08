f1b61a8d61c9316ab5e9404e5a8d87ef71762d90
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2023-04-08 17:11:31 +0000

    Update DZ9_NumericPattern.py
    update without spaces

--- DZ9_NumericPattern.py
+++ DZ9_NumericPattern.py
@@ -1,4 +1,17 @@
 n = int(input('Enter n: '))
+space=2
+for i in range(1, n+1):
+    st1=""
+    st2=""
+    for j in range(1, i):
+        st1+=str(j)+" "
+    for j in range(i,0,-1):
+        st2+=str(j)+" "
+    print(" "*(n*2 - space), st1, st2.rstrip(), sep='')
+    space += 2
+
+"""
+n = int(input('Enter n: '))
 i = 1
 j = 1
 space=1
@@ -7,4 +20,4 @@ for i in range(1, n+1):
     i += 1
     space += 2
 
-
+"""
