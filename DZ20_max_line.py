fb0180c363cc3f81d22a3d1bf97ef54fe6ba3d4a
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2023-04-18 21:07:03 +0000

    DZ20

---
+++ DZ20_max_line.py
@@ -0,0 +1,17 @@
+"""
+Есть текстовый файл. Вывести самую длинную строку.
+
+Функции max, readlines не использовать. Файл может быть большим.
+
+
+"""
+
+f = open("text.txt", "r")
+res = ""
+maxLine = 0
+for x in f:
+    if maxLine < len(x):
+        maxLine = len(x)
+    res = x
+print(res)
+f.close()
