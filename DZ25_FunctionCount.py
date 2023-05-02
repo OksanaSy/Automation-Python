daa8a8cc355024950ba292e6b7ab403e94e4e358
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2023-05-02 15:15:28 +0000

    Update DZ25_FunctionCount.py

--- DZ25_FunctionCount.py
+++ DZ25_FunctionCount.py
@@ -15,19 +15,29 @@ Function 'add' was called 1 times
 Function 'add' was called 2 times
 """
 
-counter = 0
+"""Написать декоратор с параметром.
+Если применить его к функции, он будет выводить в файл, который передали параметром,
+ сколько раз вызывалась функция на момент вызова.
+"""
+"""Написать декоратор с параметром.
+Если применить его к функции, он будет выводить в файл, который передали параметром,
+ сколько раз вызывалась функция на момент вызова.
+"""
+counter = {}
 
 
 def call_counter(file):
     def decorator_function(original_function):
         def wrapper_function(*args, **kwargs):
-            f = open(file, "a")
-            global counter
-            counter += 1
-            result = original_function(*args, **kwargs)
-            st = f"Function 'add' was called {counter} times\n"
-            f.write(st)
-            f.close()
+            with open(file, 'a') as f:
+                global counter
+                result = original_function(*args, **kwargs)
+                if (original_function.__name__ not in counter):
+                    counter[original_function.__name__] = 1
+                else:
+                    counter[original_function.__name__] += 1
+                st = f"Function {original_function.__name__} was called {counter[original_function.__name__]} times\n"
+                f.write(st)
             return result
 
         return wrapper_function
@@ -35,10 +45,19 @@ def call_counter(file):
     return decorator_function
 
 
-@call_counter('data.txt')
+@call_counter('data4.txt')
 def add(a, b):
     return a + b
 
 
+@call_counter('data4.txt')
+def mult(a, b):
+    return a * b
+
+
 print(add(4, 6))
+print(mult(4, 6))
 print(add(4, 6))
+print(mult(4, 6))
+print(add(4, 6))
+print(mult(4, 6))
