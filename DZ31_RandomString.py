e0edc0af4ce82d33486c96c7165f8015c115d9f1
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2023-05-06 22:22:53 +0300

    DZ31

---
+++ DZ31_RandomString.py
@@ -0,0 +1,23 @@
+"""Написать функцию, которая возвращает слуайную строку заданной длины.
+Строка должна состоять из больших и маленьких латинских букв и цифр.
+(снижать за это оценку не буду, но лучше постараться сделать так, чтобы символы попадали в строку равномерно)
+"""
+import random
+
+
+def get_random_string(length: int) -> str:
+    res = ''
+    for i in range(length):
+        # 48-57 digits,65-90 upper case,97-122 lower case
+        num = random.choices([random.randrange(48, 57), random.randrange(65, 90), random.randrange(97, 122)],
+                             weights=[1, 1, 1])
+        res += ''.join(chr(num[0]))
+    return res
+
+
+print(get_random_string(10))
+"""
+Ограничения:
+- Не использовать модуль string
+- Не создавать руками список ['a', 'b', 'c', ..., 'X', 'Y', 'Z', 0, 1, ..., 8, 9]
+"""
