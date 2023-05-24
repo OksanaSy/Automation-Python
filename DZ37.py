fe1f7f061bb1a7c773e655c38dfa95e4fd2e1a63
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2023-05-24 23:51:57 +0300

    Update regex DZ37.py

--- DZ37.py
+++ DZ37.py
@@ -1,12 +1,13 @@
+import re
 class AttributePrinterMixin:
     def __str__(self):
         all_members = self.__dict__.keys()
         res = type(self).__name__+ ':{\n'
         for item in all_members:
             if "__" in item:
-                res += f'\t{item.removeprefix("_"+type(self).__name__+"__")}: {self.__dict__[item]}\n'
+                res += f'\t{re.sub("^.*?__","",item)}: {self.__dict__[item]}\n'
             else:
-                res += f'\t{item.removeprefix("_")}: {self.__dict__[item]}\n'
+                res += f'\t{re.sub("^_", "", item)}: {self.__dict__[item]}\n'
         res += '}'
         return res
 
@@ -22,9 +23,6 @@ class A(AttributePrinterMixin):
 a = A()
 print(a)
 """
-
-
-
 A: {
    public_filed: 3
    protected_field: q
