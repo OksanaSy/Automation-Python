d4d54df832ac07edce832ca7665f626c8c45c9fb
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2023-05-30 18:10:30 +0000

    Update DZ38_CacheDecorator.py
    Update

--- DZ38_CacheDecorator.py
+++ DZ38_CacheDecorator.py
@@ -4,7 +4,10 @@ class cacher:
         self.function = function
 
     def __call__(self, *args, **kwargs):
-        key = str(args) + str(kwargs)
+        items = list(kwargs.keys())
+        items.sort()
+        sorted_items = {i: kwargs[i] for i in items}
+        key = str(args) + str(sorted_items)
         if key in self.cache:
             return self.cache[key]
 
