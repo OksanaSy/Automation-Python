51cfc5e5e2eed75067a22104b8faf4c564501019
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2023-06-02 19:52:49 +0000

    Update DZ39_ContextManagerTime.py

--- DZ39_ContextManagerTime.py
+++ DZ39_ContextManagerTime.py
@@ -2,31 +2,31 @@ import time
 
 
 class Timer:
+    def __init__(self):
+        self.start= time.perf_counter()
+
     def __enter__(self):
-        self.elapsed_time = time.perf_counter()
         return self
 
     def __exit__(self, type, value, traceback):
-        self.elapsed_time = time.perf_counter() - self.elapsed_time
+        self.elapsed_time = time.perf_counter() - self.start
         return self.elapsed_time
 
     def reset(self):
-        self.elapsed_time = time.perf_counter()
+        self.start = time.perf_counter()
         return self
 
-
 with Timer() as t:
-    time.sleep(1)
-print(t.elapsed_time)  # ~1 second
-time.sleep(1)
+  time.sleep(1)
+print(t.elapsed_time) # ~1 second
+time.sleep(2)
 with t:
-    time.sleep(5)
-print(t.elapsed_time)  # ~5 seconds
-
+  time.sleep(5)
+print(t.elapsed_time) # ~8 seconds
 with Timer() as t2:
-    time.sleep(3)
-print(t2.elapsed_time)  # ~3 second
+    time.sleep(1)
+print(t2.elapsed_time)  # ~1 second
 t2.reset()
 with t2:
-    time.sleep(1)
-print(t2.elapsed_time)  # ~1 seconds
+    time.sleep(2)
+print(t2.elapsed_time)  # ~2 seconds
